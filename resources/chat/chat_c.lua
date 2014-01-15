f708630dc0a961aac22d0fb71def95134fdf5998
Socialz <socialz.fin@gmail.com>
2014-01-15 13:47:58 +0200

    chat: added /clearchat command
    Clears up the chatbox, pretty much.

---
+++ resources/chat/chat_c.lua
@@ -0,0 +1,30 @@
+-- addCommandHandler supporting arrays as command names (multiple commands with the same function)
+local addCommandHandler_ = addCommandHandler
+      addCommandHandler  = function( commandName, fn, restricted, caseSensitive )
+	-- add the default command handlers
+	if type( commandName ) ~= "table" then
+		commandName = { commandName }
+	end
+	for key, value in ipairs( commandName ) do
+		if key == 1 then
+			addCommandHandler_( value, fn, restricted, caseSensitive )
+		else
+			addCommandHandler_( value,
+				function( player, ... )
+					fn( player, ... )
+				end
+			)
+		end
+	end
+end
+
+-- /clearchat
+addCommandHandler( { "clear", "clearchat" },
+	function( commandName )
+		if exports.players:isLoggedIn( localPlayer ) then
+			for i = 1, getChatboxLayout( "chat_lines" ) do
+				outputChatBox( " " )
+			end
+		end
+	end
+)
